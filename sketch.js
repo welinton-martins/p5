499f3c0de384ae46f281889a95ebb04679d49839
Welinton Martins <57647104+welinton-martins@users.noreply.github.com>
2022-12-11 14:26:16 -0300

    first sketch

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // background(220);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  
+  
+  ellipse(mouseX, mouseY, 80, 80);
+  
+}
